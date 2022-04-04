ea277cd57ad12844b6dd2b83b0dd005dfcfec32f
monacoglynn <96494895+monacoglynn@users.noreply.github.com>
2022-04-04 02:06:48 -0700

    added the conditional statement for password length and lowercase letters.

--- script.js
+++ script.js
@@ -13,7 +13,25 @@ function writePassword() {
 
   var passwordLength = Number(prompt('Choose a length between 8 and 128 characters.'));
   console.log(typeof passwordLength);
+  console.log(passwordLength)
 
+  if (passwordLength < 8) {
+    console.log('opps try again')
+    return writePassword();
+  } else if (passwordLength > 128) {
+    console.log('woah! too long')
+    return writePassword();
+  } else {
+    console.log('you picked a good length!')
+  }
+
+  var passLow = confirm('Include lowercase letters?');
+  if (passLow === true) {
+    console.log('yes please, add lowercase letters');
+  } else {
+    console,
+    log('i dont like lowercase letters')
+  }
 
 
   //  passwordText.value = password;
