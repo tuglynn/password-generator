a9eec137c8e79cecd3daba4c3dafe2e75ef7e983
monacoglynn <96494895+monacoglynn@users.noreply.github.com>
2022-04-04 00:24:21 -0700

    added a variable for password Length and converted the string into a number.

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 var lower = 'abcdefghijklmnopqrstuvwxyz';
 var upper = 'ABCDEFGHIJKLMNOPQRISTUVWXYZ';
 var numbers = '1234567890';
-var special = '?!@#$%^&*()`,.';
+var special = ' !"#$%&()*+,-./:;<=>?@[]^_`{}|~';
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  var passwordLength = Number(prompt('Choose a length between 8 and 128 characters.'));
+  console.log(typeof passwordLength);
 
+
+
+  //  passwordText.value = password;
 }
+
+writePassword();
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
