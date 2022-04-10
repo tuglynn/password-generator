68b481b6ec5efb5463e3e20c35daf55eada164df
monacoglynn <96494895+monacoglynn@users.noreply.github.com>
2022-04-09 17:15:39 -0700

    added a click to copy function once the password is generated. there is nothing visible on the page to show yet.

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,6 +1,6 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate"); // this lets us select the generate password button
-
+let generateBtn = document.querySelector("#generate"); // this lets us select the generate password button
+let passwordText = document.querySelector('#password');
 //these are my string with all possible characters for the password.
 const lower = 'abcdefghijklmnopqrstuvwxyz';
 const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -12,7 +12,7 @@ const symbols = ' !#$%&()*+,-./:;<=>?@[]^_`{}|~';
 function writePassword() {
     // let password = generatePassword(); // This runs our randomized loop that constructs our password.
     let passwordString = '';
-    let passwordText = document.querySelector("#password"); // this allows us to select the password text box
+    let passwordText = document.querySelector('#password'); // this allows us to select the password text box
     // this grabs the length of the desired password from the user.
     const passwordLength = Number(prompt('Please enter your desired password length between 8 - 128 characters.'));
     if (passwordLength >= 8 && passwordLength <= 128) {
@@ -61,6 +61,17 @@ function createUserString() {
 function rand(stringLength) {
     return Math.floor(Math.random() * stringLength);
 }
+// this function will copy the text inside the password area.
+function copyPassword(event) {
+    let passwordText = document.querySelector('#password');
+    //this method is similar to highlighting text with our mouse
+    passwordText.select();
+    //this actually copies to clipboard.
+    document.execCommand('copy');
+    console.log('copied')
+}
+
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
+passwordText.addEventListener('click', copyPassword);
